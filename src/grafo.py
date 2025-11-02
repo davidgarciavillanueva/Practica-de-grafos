ca776d6fbb12e95fdfc5734e15b9a08f54bec7fa
Sebastian Villa <sebasviro567@gmail.com>
2025-11-02 14:17:21 -0500

    Creacion y correccion

--- src/grafo.py
+++ src/grafo.py
@@ -1,13 +1,26 @@
-class Grafo:
-    def __init__(self, dirigido=False):
-        self.nodos = {}
-        self.aristas = []
-        self.dirigido = dirigido
+class GraphList:
     
-    def agregar_nodo(self, id_nodo, nombre, ubicacion):
+    def __init__(self, dirigido=False):
+        self.adj_list = {}
+        self.pesos = {}
+        self.info_nodos = {}
+        self.size = 0
+        self.dirigido = dirigido
+
+    def agregar_nodo(self, value, nombre=""):
         """Agrega un nodo al grafo"""
-        self.nodos[id_nodo] = {
-            'nombre': nombre,
-            'ubicacion': ubicacion
-        }
+        if value in self.adj_list:
+            return None
+        self.adj_list[value] = []
+        self.info_nodos[value] = {'nombre': nombre}
+        self.size += 1
+
+    def agregar_arista(self, vertex_1, vertex_2, distancia, costo_combustible):
+        """Agrega una arista con peso calculado"""
+        if vertex_1 not in self.adj_list:
+            self.agregar_nodo(vertex_1)
+        if vertex_2 not in self.adj_list:
+            self.agregar_nodo(vertex_2)
+
+    
     
