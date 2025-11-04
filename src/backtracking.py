b21ce7b2751fe774ade6ae482ca4e543e83ea150
davidgarciavillanueva <davidgarciavillanueva15@gmail.com>
2025-11-04 08:38:24 -0500

    Refactorizar la clase

--- src/backtracking.py
+++ src/backtracking.py
@@ -8,14 +8,37 @@ class OptimizadorRutas:
         nodos = self.grafo.obtener_nodos()
         nodos.remove(inicio)
         
-        # Pila para simular la recursión: (ruta, visitados, costo, nodos_restantes)
-        pila = [(
-            [inicio],           # ruta_actual
-            {inicio},           # visitados
-            0,                  # costo_actual
-            nodos.copy()        # nodos_restantes
-        )]
+        # Pila (ruta act, visitados, costo act, nodos_restantes)
+        pila = [([inicio],{inicio},0,nodos.copy())]
         
         while pila:
             ruta_actual, visitados, costo_actual, nodos_restantes = pila.pop()
+            
+            # Caso base: todos los nodos visitados
+            if not nodos_restantes:
+                costo_regreso = self.grafo.obtener_peso(ruta_actual[-1], inicio)
+                costo_total = costo_actual + costo_regreso
+                
+                if costo_total < self.mejor_costo:
+                    self.mejor_costo = costo_total
+                    self.mejor_ruta = ruta_actual + [inicio]
+                continue  
+            
+            for nodo in reversed(nodos_restantes):
+                costo_arista = self.grafo.obtener_peso(ruta_actual[-1], nodo)
+                nuevo_costo = costo_actual + costo_arista
+                
+                if nuevo_costo < self.mejor_costo:
+                    nueva_ruta = ruta_actual + [nodo]
+                    nuevos_visitados = visitados | {nodo}
+                    nuevos_nodos_restantes = [n for n in nodos_restantes if n != nodo]
+                    
+                    pila.append((
+                        nueva_ruta,
+                        nuevos_visitados,
+                        nuevo_costo,
+                        nuevos_nodos_restantes
+                    ))
+        
+        return self.mejor_ruta, self.mejor_costo
 
