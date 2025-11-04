27157386fb8c8de6cdebd729981755afd31115ac
davidgarciavillanueva <davidgarciavillanueva15@gmail.com>
2025-11-03 19:14:38 -0500

    quitar lineas repetidas y que no se usan

--- src/grafo.py
+++ src/grafo.py
@@ -1,28 +1,24 @@
 class GraphList:
-    
     def __init__(self, dirigido=False):
         self.adj_list = {}
         self.pesos = {}
-        self.info_nodos = {}
         self.size = 0
         self.dirigido = dirigido
 
-    def agregar_nodo(self, value, nombre=""):
-        """Agrega un nodo al grafo"""
+    def agregar_nodo(self, value):
         if value in self.adj_list:
             return None
         self.adj_list[value] = []
-        self.info_nodos[value] = {'nombre': nombre}
         self.size += 1
 
     def agregar_arista(self, vertex_1, vertex_2, distancia, costo_combustible):
-        """Agrega una arista con peso calculado"""
         if vertex_1 not in self.adj_list:
             self.agregar_nodo(vertex_1)
         if vertex_2 not in self.adj_list:
             self.agregar_nodo(vertex_2)
 
-        # Agregar arista
+        peso = (distancia / 10) * costo_combustible
+        
         if vertex_2 not in self.adj_list[vertex_1]:
             self.adj_list[vertex_1].append(vertex_2)
             self.pesos[(vertex_1, vertex_2)] = peso
@@ -30,36 +26,18 @@ class GraphList:
         if not self.dirigido and vertex_1 not in self.adj_list[vertex_2]:
             self.adj_list[vertex_2].append(vertex_1)
             self.pesos[(vertex_2, vertex_1)] = peso
-        
-         # Calcular peso
-        peso = self._calcular_peso(distancia, costo_combustible)
-
-    
-    def _calcular_peso(self, distancia, costo_combustible):
-        """Calcula el peso basado en distancia y combustible"""
-        galones = distancia / 10
-        return galones * costo_combustible
-
 
     def obtener_vecinos(self, vertex):
-        """Retorna lista de (vecino, peso)"""
-        vecinos = []
-        for neighbor in self.adj_list[vertex]:
-            peso = self.pesos.get((vertex, neighbor), float('inf'))
-            vecinos.append((neighbor, peso))
-        return vecinos
-
+        return [(neighbor, self.pesos.get((vertex, neighbor), float('inf'))) 
+                for neighbor in self.adj_list[vertex]]
 
     def obtener_peso(self, vertex_1, vertex_2):
-        """Obtiene el peso entre dos nodos"""
         return self.pesos.get((vertex_1, vertex_2), float('inf'))
 
     def obtener_nodos(self):
-        """Retorna todos los nodos del grafo"""
         return list(self.adj_list.keys())
 
     def obtener_aristas(self):
-        """Retorna todas las aristas con su información"""
         aristas = []
         for origen in self.adj_list:
             for destino in self.adj_list[origen]:
@@ -69,11 +47,4 @@ class GraphList:
                         'destino': destino,
                         'peso': self.pesos[(origen, destino)]
                     })
-                elif self.dirigido:
-                    aristas.append({
-                        'origen': origen,
-                        'destino': destino,
-                        'peso': self.pesos[(origen, destino)]
-                    })
         return aristas
-
