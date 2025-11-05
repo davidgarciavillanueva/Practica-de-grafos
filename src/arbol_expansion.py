31aefc02297618d4408694125e654d488f5a8b9d
davidgarciavillanueva <davidgarciavillanueva15@gmail.com>
2025-11-04 23:22:17 -0500

    Update arbol_expansion.py

--- src/arbol_expansion.py
+++ src/arbol_expansion.py
@@ -24,31 +24,32 @@ class UnionFind:
                 self.rank[root_x] += 1
             return True
         return False
+        
+def kruskal(grafo):
+    """Algoritmo de Kruskal para árbol de expansión mínimo"""
+    # Mapear nodos a índices
+    nodos = grafo.obtener_nodos()
+    nodo_a_indice = {nodo: i for i, nodo in enumerate(nodos)}
 
-    def kruskal(grafo):
-        """Algoritmo de Kruskal para árbol de expansión mínimo"""
-        # Mapear nodos a índices
-        nodos = grafo.obtener_nodos()
-        nodo_a_indice = {nodo: i for i, nodo in enumerate(nodos)}
+    # Ordenar aristas por peso
+    aristas_ordenadas = sorted(grafo.obtener_aristas(), key=lambda x: x['peso'])
 
-        # Ordenar aristas por peso
-        aristas_ordenadas = sorted(grafo.obtener_aristas(), key=lambda x: x['peso'])
+    uf = UnionFind(len(nodos))
+    arbol_expansion = []
+    costo_total = 0
 
-        uf = UnionFind(len(nodos))
-        arbol_expansion = []
-        costo_total = 0
+    for arista in aristas_ordenadas:
+        if len(arbol_expansion) == len(nodos) - 1:
+            break
 
-        for arista in aristas_ordenadas:
-            if len(arbol_expansion) == len(nodos) - 1:
-                break
+        origen_idx = nodo_a_indice[arista['origen']]
+        destino_idx = nodo_a_indice[arista['destino']]
 
-            origen_idx = nodo_a_indice[arista['origen']]
-            destino_idx = nodo_a_indice[arista['destino']]
+        if uf.union(origen_idx, destino_idx):
+            arbol_expansion.append(arista)
+            costo_total += arista['peso']
 
-            if uf.union(origen_idx, destino_idx):
-                arbol_expansion.append(arista)
-                costo_total += arista['peso']
+    return arbol_expansion, costo_total
 
-        return arbol_expansion, costo_total
 
   
